eca658b1d8b054cabdbd52bf844b57b8832624a5
Harshit010 <72172598+Harshit010@users.noreply.github.com>
2020-10-01 06:51:46 +0530

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main(){
+int a ; cin>> a;
+int  b; cin>> b;
+double average = (a+b)/2.0;
+cout << average;
+return 0;
+}
