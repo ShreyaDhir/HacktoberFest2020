1e16b1ebc5cbfd73178900ae207cd3af7bab34c1
lakshya3841 <62415196+lakshya3841@users.noreply.github.com>
2020-10-09 12:34:46 +0000

    Update A_Juggling_Letters.cpp
    See the commented line .

--- A_Juggling_Letters.cpp
+++ A_Juggling_Letters.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 int main()
 {
+    // Earlier your program didn't use to ask for number's so here's my helping hand
     int t;
     cin >> t;
     while (t--)
     {
+        cout<<"Enter a number please"<<endl;
         int n;
         cin >> n;
         int count[26] = {0};
+        t--;
 
         for (int i = 0; i < n; i++)
         {
+            cout<<"Enter a number please"<<endl;
             string s;
             cin >> s;
             for (char ch : s)
