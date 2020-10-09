134cad899638049ef290b043bfdb6a615f6b9c98
lakshya3841 <62415196+lakshya3841@users.noreply.github.com>
2020-10-09 12:43:56 +0000

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-  cout<<"Hello World."<<endl;
-  cout<<"everyone can ask for pull requests...";<<end2
-  cout<<"Wow this is cool! First time adding something to something!"<<end3
-  cout<<"Hacktoberfest 2020 is on....."<<end4
+  string name;
+  cin>>name;
+  cout<<"Hello "<<name<<endl;
+/*  cout<<"everyone can ask for pull requests...\n";
+  cout<<"Wow this is cool! First time adding something to something!\n"
+  cout<<"Hacktoberfest 2020 is on.....\n"   */
   //Add any fancy code you want! All pull requests are welcomed!
   return 0;
 }
