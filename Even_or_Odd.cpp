a3a04cbe59ecdd17cce6abe8a2d4311325058cf1
Shashikala <shashikala1631@gmail.com>
2020-10-17 16:03:46 +0530

    added even or odd algorithm

---
+++ Even_or_Odd.cpp
@@ -0,0 +1,13 @@
+//Check whether number is even or odd
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n&1)
+        cout<<"Odd";
+    else
+        cout<<"Even";
+    return 0;
+}
